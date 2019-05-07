d5083d8667ce1a0c404f354cea614892de4157d5
Rygel-XVI <micha232@umn.edu>
2019-05-07 14:37:20 -0700

    changed SUITS and NUMBERS from const to variables inside Deck#create_deck

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,13 +1,12 @@
 class Deck < ApplicationRecord
   has_many :cards
 
-  SUITS = ["Hearts", "Spades", "Clubs", "Diamonds"]
-  NUMBERS = [1,2,3,4,5,6,7,8,9,10,11,12,13]
-
   def self.create_deck
+    suits = ["Hearts", "Spades", "Clubs", "Diamonds"]
+    numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13]
     deck = Deck.create!
-    SUITS.each do |s|
-      NUMBERS.each do |n|
+    suits.each do |s|
+      numbers.each do |n|
         deck.cards << Card.create!(suit: s, number: n, deck_id: deck.id)
       end
     end
