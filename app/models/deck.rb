7b6de5ff603e612b5b29d982d22100b99f8fb962
Rygel-XVI <micha232@umn.edu>
2019-05-07 10:38:07 -0700

    added prelim validations for model creation

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,11 @@
 class Deck < ApplicationRecord
   has_many :cards
+
+  SUITS = ["Hearts", "Spades", "Clubs", "Diamonds"]
+  NUMBERS = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+
+  def initialize
+    binding.pry
+  end
+
 end
