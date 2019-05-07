08b5e3a95be9cbc5e232279e0937624a9789c982
Rygel-XVI <micha232@umn.edu>
2019-05-07 15:22:57 -0700

    fixed names to be consistent in deck#create

--- app/controllers/api/deck_controller.rb
+++ app/controllers/api/deck_controller.rb
@@ -5,17 +5,11 @@ class Api::DeckController < ApplicationController
 
   def create
     deck = Deck.create!
-    d.create_cards
+    deck.create_cards
     render json: deck.cards
   end
 
   def destroy
-  end
-
-private
-
-  def deck_params
-    params.require(:deck).permit(:id)
   end
 
 end
